d4b96169ba0c34bd81e9f42f11f7e55e6c890698
Yazeed <dee3ayahmed25@gmail.com>
2023-07-03 15:14:17 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /birds
   def index
@@ -14,45 +15,30 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    bird = find_bird
       render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+
   end
 
   # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    bird =  find_bird
       bird.update(bird_params)
       render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
   end
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    bird = find_bird
       bird.update(likes: bird.likes + 1)
       render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
   end
 
   # DELETE /birds/:id
   def destroy
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    bird =find_bird
       bird.destroy
       head :no_content
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
   end
 
   private
@@ -60,5 +46,13 @@ class BirdsController < ApplicationController
   def bird_params
     params.permit(:name, :species, :likes)
   end
+
+  def  render_not_found_response
+  render json: { error: "Bird not found" }, status: :not_found
+end
+
+def find_bird
+  Bird.find_by(id: params[:id])
+end
 
 end
